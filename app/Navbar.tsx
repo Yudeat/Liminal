9401b35d44e688159906066b597009f985e5ad68
Yudeat <ayushtiwaei0@gmail.com>
2026-01-12 21:54:03 +0545

    Working on nav

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -2,13 +2,22 @@ import Image from "next/image";
 export default function Navbar() {
   return (
     <>
-            <Image
-              src="/logo.png"
-              alt="logo"
-              width={100}
-              height={100}
-              className=""
-            />
+    <div className="flex flex-row items-center">
+      <div className="flex flex-row  text-4xl items-center">
+         <Image src="/logoo.png" 
+       width={200}
+       height={200}
+       alt="logo" 
+       className="ml-5 
+       h-10 
+     rounded-full
+     w-24
+     h-auto
+     "
+      />
+      </div>
+   
+      </div>
             <h1 className="text-4xl font-bold ml-5">Liminal</h1>    
 </>
           
