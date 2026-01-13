c591455cce69cfa33a44a2425be77ecc590c74bd
Yudeat <ayushtiwaei0@gmail.com>
2026-01-13 23:27:14 +0545

    Navbar fixed

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,19 +1,26 @@
 import ParticlesBackground from "@/components/lightswind/particles-background";
 import Image from "next/image";
+import SparkleNavbar  from '@/components/lightswind/sparkle-navbar'
 
 export default function Navbar() {
   return (
-    <nav className="relative flex flex-row items-center w-full h-24 px-6 overflow-hidden">
- <ParticlesBackground />
-      <div className="flex items-center">
-        <Image
-          src="/log.png"
-          alt="logo"
-          width={200}
-          height={60} 
-          priority 
-        />
-      </div>
-    </nav>
+ <>
+ <div className="  w-full h-24 flex items-center justify-center">
+    <SparkleNavbar
+  items={['Home', 'About', 'Services', 'Contact']}
+  color="#1E90FF"
+/>
+</div>
+      <ParticlesBackground
+  colors={['#00ffff', '#ff00ff', '#ffaa00']}
+  size={4}
+  countDesktop={80}
+  countTablet={60}
+  countMobile={40}
+  zIndex={-1}
+  height="100vh"
+/>
+
+</>
   );
 }
