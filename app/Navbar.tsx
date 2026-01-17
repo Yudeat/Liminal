6ac133b7495eedf882eb14b8f26f2101dfb041ae
Yudeat <ayushtiwaei0@gmail.com>
2026-01-17 18:32:40 +0545

    Animated hero text

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -4,8 +4,32 @@ import { GiHamburgerMenu } from "react-icons/gi";
 import { FaRegCircleUser } from "react-icons/fa6";
 import SparkleNavbar from '@/components/lightswind/sparkle-navbar';
 import Hyperspeed from "@/components/Hyperspeed";
+import {motion} from "framer-motion";
+  const fadeUp = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1, 
+  transition: { 
+  staggerChildren: 0.3,
+  delayChildren: 0.2,
+  } },
+};
+
+const itemAnimate={
+  hidden: { opacity: 0, y: 30 },
+  visible:{
+    opacity: 1,
+    y: 0,
+    transition: { 
+type: 'tween', 
+ease: 'easeOut', 
+duration: 1.2,
+
+}
+  }
+}
 
 export default function LandingPage() {
+
   return (
     <div className="relative min-h-screen w-full bg-black text-white">
       
@@ -52,7 +76,7 @@ export default function LandingPage() {
         />
       </div>
 
-      <div className="relative z-10">
+      <div className="relative z-10" >
         
         <nav className="relative mx-4 mt-4 max-w-7xl lg:mx-auto border p-2 md:p-4 h-auto md:h-24 rounded-3xl md:rounded-full flex flex-col md:flex-row items-center justify-evenly bg-black/40 backdrop-blur-md overflow-hidden text-white border-white/10">
           
@@ -65,7 +89,7 @@ export default function LandingPage() {
           {/* Middle Links */}
           <div className="w-full md:w-auto md:block hidden flex items-center justify-center z-10 py-2 md:py-0">
             <SparkleNavbar
-              items={['Home', 'About', 'Services', 'Contact']}
+              items={['Home' , 'Services','Docs','About', 'Contact']}
               color="#2CFF05"
             />
           </div>
@@ -83,17 +107,27 @@ export default function LandingPage() {
         </nav>
 
        
-        <header className="flex flex-col items-center justify-center text-center px-4 pt-52 pb-20">
-          <h1 className="text-xl md:text-5xl font-black tracking-tighter mb-6 bg-clip-text text-transparent bg-gradient-to-b from-pink-600 to-gray-500">
+        <motion.div className="flex flex-col items-center justify-center text-center px-4 pt-52 pb-20" variants={fadeUp} 
+        whileInView="visible"
+        initial="hidden" animate="visible" >
+          <motion.h1 
+          variants={itemAnimate}
+          className="text-xl md:text-5xl font-black tracking-tighter mb-6 bg-clip-text text-transparent bg-gradient-to-b from-pink-600 to-gray-500">
           Everything you need to apply abroad verified, guided, and in your control.
-          </h1>
-          <p className="max-w-2xl text-lg md:text-xl text-gray-400 mb-10">
+          </motion.h1>
+          <motion.p 
+          variants={itemAnimate}
+          className="max-w-2xl text-lg md:text-xl text-gray-400 mb-10">
            Liminal replaces confusing consultancy processes with a transparent system that
-            checks your eligibility, verifies your documents, and guides every step — while you stay in charge of submission.          </p>
-          <button className="px-8 py-4 bg-[#bf00ff] text-[#2cff05] font-bold rounded-full hover:bg-pink-400 transition-colors">
+            checks your eligibility, verifies your documents, and guides every step — while you stay in charge of submission.          </motion.p>
+          <motion.button
+          variants={itemAnimate}
+          whileHover={{scale:1.05}}
+          whileTap={{scale:0.95}}
+          className="px-8 py-4 bg-[#bf00ff] text-[#2cff05] font-bold rounded-full hover:bg-pink-400 transition-colors">
             Get Started
-          </button>
-        </header>
+          </motion.button>
+        </motion.div>
 
       </div>
     </div>
