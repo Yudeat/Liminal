19076693ef51ed757fde625d228c2a6d4044ccad
Yudeat <ayushtiwaei0@gmail.com>
2026-01-28 22:24:36 +0545

    Newsletter and footer

---
+++ app/Newsletter.tsx
@@ -0,0 +1,9 @@
+export default function Newsletter (){
+    return (
+        <>
+        <div>
+            <h1 className="text-white">ass</h1>
+        </div>
+        </>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Navbar from "./Navbar";
 import {ModeToggle}  from "./Main";
+import { Newspaper } from "lucide-react";
 export default function Home() {
   return (<>
   {/* main component */}
  <Navbar />
 <ModeToggle />
-
+<Newspaper/>
     
 
     </>
