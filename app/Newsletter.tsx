13e7bc2d62175ba2c6404efd08a6ab2199524a0f
Yudeat <ayushtiwaei0@gmail.com>
2026-02-04 22:13:06 +0545

    sosmetging

--- app/Newsletter.tsx
+++ app/Newsletter.tsx
@@ -1,8 +1,8 @@
 export default function Newsletter (){
     return (
         <>
-        <div className="">
-           Suscribe to Newsletter 
+        <div className="max-w-2xl text-lg md:text-xl text-gray-400 mb-10">
+           Subscribe to our newsletter to get the latest updates and exclusive offers.
         </div>
         </>
     )
