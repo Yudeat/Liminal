b013da219aecca7c2fc9063f9a2bccc6fa2f483b
Yudeat <ayushtiwaei0@gmail.com>
2026-02-05 23:09:49 +0545

    some changes

--- app/Newsletter.tsx
+++ app/Newsletter.tsx
@@ -1,7 +1,7 @@
 export default function Newsletter (){
     return (
         <>
-        <div className="max-w-2xl text-lg md:text-xl text-gray-400 mb-10">
+        <div className="max-w-2xl text-lg md:text-xl text-gray-400 border-b border-gray-200 pb-4 mb-4">
            Subscribe to our newsletter to get the latest updates and exclusive offers.
         </div>
         </>
